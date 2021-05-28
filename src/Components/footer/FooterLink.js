378fa88f46758aa5f52499bdabd0824c83ea0ea5
Ziping Liu <herrickinjilincity@gmail.com>
2021-05-28 19:51:11 -0400

    code review on Home Page

--- src/Components/footer/FooterLink.js
+++ src/Components/footer/FooterLink.js
@@ -5,7 +5,7 @@ import styles from "./FooterLink.module.css";
 
 const FooterLink = (props) => {
   return (
-    <div>
+    <div className={styles.footer_link}>
       <a href="/#">{props.name}</a>
       {props.children ? (
         <div className={styles.icon_container}>{props.children}</div>
