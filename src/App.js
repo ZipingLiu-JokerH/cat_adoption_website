34f0a51fb28f2260330595096d3ebd2ce9914d81
Ziping Liu <herrickinjilincity@gmail.com>
2021-06-16 21:20:12 -0400

    adding lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 //components
 import { Route, Switch } from "react-router-dom";
@@ -6,37 +6,40 @@ import { CatsContextProvider } from "./CatsContext";
 import Footer from "./Components/footer/Footer";
 import Header from "./Components/header/Header";
 import Home from "./Pages/Home";
-import Donate from "./Pages/Donate";
-import AvaliableCats from "./Pages/AvaliableCats";
-import SingleCat from "./Pages/SingleCat";
-import AdoptionProcess from "./Pages/AdoptionProcsss";
+
+const Donate = React.lazy(() => import("./Pages/Donate"));
+const AvaliableCats = React.lazy(() => import("./Pages/AvaliableCats"));
+const SingleCat = React.lazy(() => import("./Pages/SingleCat"));
+const AdoptionProcess = React.lazy(() => import("./Pages/AdoptionProcsss"));
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CatsContextProvider />
-      <Switch>
-        <Route path="/adoption-process/:name">
-          <AdoptionProcess />
-        </Route>
-        <Route path="/single-cat/:name">
-          <SingleCat />
-        </Route>
-        <Route path="/avaliable-cats">
-          <AvaliableCats />
-        </Route>
-        <Route path="/donate">
-          <Donate />
-        </Route>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="*">
-          <div className="page_container">404 page</div>
-        </Route>
-      </Switch>
-      <CatsContextProvider />
+      <CatsContextProvider>
+        <Suspense fallback={<div className="page_container">Loading...</div>}>
+          <Switch>
+            <Route path="/adoption-process/:name">
+              <AdoptionProcess />
+            </Route>
+            <Route path="/single-cat/:name">
+              <SingleCat />
+            </Route>
+            <Route path="/avaliable-cats">
+              <AvaliableCats />
+            </Route>
+            <Route path="/donate">
+              <Donate />
+            </Route>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="*">
+              <div className="page_container">404 page</div>
+            </Route>
+          </Switch>
+        </Suspense>
+      </CatsContextProvider>
       <Footer />
     </div>
   );
